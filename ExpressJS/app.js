b5bcc5318a6c67c9b4632058c3a93ba3cc6f49b1
kafadarova <k.kafadarova@gmail.com>
2018-09-29 23:16:10 +0200

    Add a html ejs template and add views folder

--- ExpressJS/app.js
+++ ExpressJS/app.js
@@ -13,18 +13,26 @@ const app = express();
 // // Call the custom middleware called logger
 // app.use(logger);
 
+// View Engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 // Add body parses middleware to handle parsing json content
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 // Set Static Path
-app.use(express.statis(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname,'public')));
+
+
 
 // Routes
 // Visit an app/website is a GET request
 // Submitting a form - POST request
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  // res.json(person);
+  // res.send('Hello World');
+  res.render('index');
 });
 
 // Listen to a port & set a callback function
