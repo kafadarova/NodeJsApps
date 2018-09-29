c07ae1cade04d5eedaf8ca0d572810b92810ff8e
kafadarova <k.kafadarova@gmail.com>
2018-09-30 00:01:01 +0200

    Create a submission form and take the data from it after its submission

--- ExpressJS/app.js
+++ ExpressJS/app.js
@@ -24,7 +24,26 @@ app.use(bodyParser.urlencoded({extended: false}));
 // Set Static Path
 app.use(express.static(path.join(__dirname,'public')));
 
-
+let users = [
+{
+  id: 1,
+  first_name: 'Kristin',
+  last_name: 'Kafadarova',
+  email: 'kkafadarova@gmail.com'
+},
+{
+  id: 2,
+  first_name: 'Eva',
+  last_name: 'Kafadarova',
+  email: 'ekafadarova@gmail.com'
+},
+{
+  id: 3,
+  first_name: 'Mariya',
+  last_name: 'Kafadarova',
+  email: 'mkafadarova@gmail.com'
+}
+];
 
 // Routes
 // Visit an app/website is a GET request
@@ -32,7 +51,20 @@ app.use(express.static(path.join(__dirname,'public')));
 app.get('/', (req, res) => {
   // res.json(person);
   // res.send('Hello World');
-  res.render('index');
+  res.render('index', {
+    title: 'Customers',
+    users: users
+  });
+});
+
+// Catch the submisson of the form from the index html
+app.post('/users/add',(req,res) => {
+  let newUser = {
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    email: req.body.email
+  };
+  console.log(newUser);
 });
 
 // Listen to a port & set a callback function
