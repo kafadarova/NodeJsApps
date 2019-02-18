e48548116b118ffd9f6880532355db3db46b99d2
kafadarova <k.kafadarova@gmail.com>
2019-02-18 22:45:50 +0100

    create a user module

---
+++ DevConnector/models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// create Schema
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    require: true
+  },
+  email: {
+    type: String,
+    require: true
+  },
+  password: {
+    type: String,
+    require: true
+  },
+  avatar: {
+    type: String
+  },
+  date: {
+    type: Date,
+    default: Date.now()
+  }
+});
+
+module.exports = User = mongoose.model('users', UserSchema);
+
+// const User = mongoose.model('User', UserSchema);
+// module.exports = User;
