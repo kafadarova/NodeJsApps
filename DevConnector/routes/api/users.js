5164ddf15ac44401a7f3b305b439a92cc19e9362
kafadarova <k.kafadarova@gmail.com>
2019-02-19 22:50:16 +0100

    create a login route and save the new user into the db

--- DevConnector/routes/api/users.js
+++ DevConnector/routes/api/users.js
@@ -1,12 +1,54 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+
+// load User model
+const User = require('../../models/User');
 
 // @route   GET to api/users/test
 // @desc    Tests user routes
 // @access  Public
-router.get('/test', (req, res) =>{
+router.get('/test', (req, res) => {
   res.json({msg: "Users works"});  // output json
 });
+
+
+// @route   GET to api/users/register
+// @desc    Register user 
+// @access  Public
+router.post('/register', (req, res) => {
+  User.findOne({ email: req.body.email})  // looking for a record that has the email 
+  .then(user => {
+    if(user) {
+      return res.status(400).json({email: 'Email already exists'});
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: '200', // size
+        r: 'pg', // rating
+        d: 'mm' // default
+      });
+      const newUser = new User({  //create a resource with Moongoose  - new and then the model name
+        name: req.body.name,
+        email: req.body.email,
+        avatar,  // avatar: avatar
+        password: req.body.password
+      });
+      
+      // generate salt
+      bcrypt.genSalt(10, (err, salt) => { // err or salt
+        bcrypt.hash(newUser.password, salt, (err, hash) => { // err or hash
+          if(err) throw err;
+          newUser.password = hash;
+          newUser.save()
+          .then(user => res.json(user)) // send a successful response with the new user
+          .catch(err => console.log(err)); // log the error if any
+        })
+      })
+    }
+  })
+});
+
 
 module.exports = router;
 
--- DevConnector/server.js
+++ DevConnector/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 // Routes 
 const users = require('./routes/api/users');
@@ -8,6 +9,9 @@ const profile = require('./routes/api/profile');
 
 const app = express();
 
+// Body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 // DB config
 const db = require('./config/keys').mongoURI;
