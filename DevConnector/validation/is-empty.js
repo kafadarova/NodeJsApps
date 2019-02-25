14f868f045b2d2ec0a791fca0a39daf31955a375
kafadarova <k.kafadarova@gmail.com>
2019-02-25 22:51:29 +0100

    refactor the code for isempty function and export it so we can use it

--- DevConnector/validation/is-empty.js
+++ DevConnector/validation/is-empty.js
@@ -1,8 +1,9 @@
-function isEmpty(value) {
-  return(
+const isEmpty = value => 
     value === undefined ||
     value === null ||
     (typeof value === 'object' && Object.keys(value).length === 0) ||  // if there arent any keys - empty object
-    (typeof value === 'string' && value.trim().length === 0)
-  );
-} 
+    (typeof value === 'string' && value.trim().length === 0);
+    
+    // export it so we can use it 
+    
+    module.exports - isEmpty;
