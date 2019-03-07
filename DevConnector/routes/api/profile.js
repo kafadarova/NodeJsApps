bb77b634d84c4093ff1b9a9456283decec50cb67
kafadarova <k.kafadarova@gmail.com>
2019-03-07 22:50:46 +0100

    add a new route for education

--- DevConnector/routes/api/profile.js
+++ DevConnector/routes/api/profile.js
@@ -6,6 +6,8 @@ const passport = require('passport');
 // Load Validation
 const validateProfileInput = require('../../validation/profile');
 const validateExperienceInput = require('../../validation/experience');
+const validateEducationInput  = require('../../validation/education');
+
 
 // Load Profile Model
 const Profile = require('../../models/Profile');
@@ -174,6 +176,35 @@ router.post('/experience', passport.authenticate('jwt', { session: false }), (re
     }
     // Add to exp array
     profile.experience.unshift(newExp); // add the exps to the begging of the array
+    profile.save().then(profile => res.json(profile));
+  })
+});
+
+// @route   Post to api/profile/education
+// @desc    Add education to profile
+// @access  Private
+router.post('/education', passport.authenticate('jwt', { session: false }), (req,res) => {
+  const {errors, isValid} = validateEducationInput(req.body);
+  
+  //Check Validation
+  if (!isValid) {
+    // Return any errors with 400 status
+    return res.status(400).json(errors);
+  }
+  
+  Profile.findOne({ user: req.user.id})
+  .then(profile => {
+    const newEdu = { // new experience
+      school: req.body.school,
+      degree: req.body.degree,
+      fieldofstudy: req.body.fieldofstudy,
+      from: req.body.from,
+      to: req.body.to,
+      current: req.body.current,
+      description: req.body.description
+    }
+    // Add to exp array
+    profile.education.unshift(newEdu); // add the education to the begging of the array
     profile.save().then(profile => res.json(profile));
   })
 });
