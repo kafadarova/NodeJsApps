df70f3bb5572beebcfd1c84b2bc1eb4cd1347cd4
kafadarova <k.kafadarova@gmail.com>
2019-03-07 22:56:33 +0100

    add a delete route

--- DevConnector/routes/api/profile.js
+++ DevConnector/routes/api/profile.js
@@ -208,5 +208,29 @@ router.post('/education', passport.authenticate('jwt', { session: false }), (req
     profile.save().then(profile => res.json(profile));
   })
 });
+
+
+// @route   Delete api/profile/experience/:exp_id
+// @desc    Delete experience FROM profile
+// @access  Private
+router.post('/experience/:exp_id', passport.authenticate('jwt', { session: false }), (req,res) => {
+  
+  Profile.findOne({ user: req.user.id})
+  .then(profile => {
+    // find the experience which we want to Delete - Ger remove index
+    const removeIndex = profile.experience
+    .map(item => item.id)
+    .indexOf(req.params.exp_id);
+    
+    // Splice out of array
+    profile.experience.splice(removeIndex, 1);
+    
+    // save
+    profile.save().then(profile => res.json(profile));
+  })
+  .catch(err => res.status(404).json(err));
+});
+
+
 module.exports = router;
 
