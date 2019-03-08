8482546a10da5d704343621b415dc92ab6389dd4
kafadarova <k.kafadarova@gmail.com>
2019-03-08 22:36:31 +0100

    delete an education

--- DevConnector/routes/api/profile.js
+++ DevConnector/routes/api/profile.js
@@ -213,7 +213,7 @@ router.post('/education', passport.authenticate('jwt', { session: false }), (req
 // @route   Delete api/profile/experience/:exp_id
 // @desc    Delete experience FROM profile
 // @access  Private
-router.post('/experience/:exp_id', passport.authenticate('jwt', { session: false }), (req,res) => {
+router.delete('/experience/:exp_id', passport.authenticate('jwt', { session: false }), (req,res) => {
   
   Profile.findOne({ user: req.user.id})
   .then(profile => {
@@ -224,6 +224,27 @@ router.post('/experience/:exp_id', passport.authenticate('jwt', { session: false
     
     // Splice out of array
     profile.experience.splice(removeIndex, 1);
+    
+    // save
+    profile.save().then(profile => res.json(profile));
+  })
+  .catch(err => res.status(404).json(err));
+});
+
+// @route   Delete api/profile/education/:edu_id
+// @desc    Delete education FROM profile
+// @access  Private
+router.delete('/education/:edu_id', passport.authenticate('jwt', { session: false }), (req,res) => {
+  
+  Profile.findOne({ user: req.user.id})
+  .then(profile => {
+    // find the experience which we want to Delete - Ger remove index
+    const removeIndex = profile.education
+    .map(item => item.id)
+    .indexOf(req.params.edu_id);
+    
+    // Splice out of array
+    profile.education.splice(removeIndex, 1);
     
     // save
     profile.save().then(profile => res.json(profile));
