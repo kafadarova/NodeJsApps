00e026b213b75799d9531373f8c80d5694ab91e2
kafadarova <k.kafadarova@gmail.com>
2019-03-10 19:30:42 +0100

    add routes for like/dislike posts

--- DevConnector/routes/api/posts.js
+++ DevConnector/routes/api/posts.js
@@ -80,5 +80,55 @@ router.delete('/:id',passport.authenticate('jwt',{ session: false }), (req,res) 
   })
 });
 
+// @route   Post to api/posts/like/:id
+// @desc    Like post
+// @access  Private
+router.post(
+  '/like/:id',passport.authenticate('jwt',{ session: false }), (req,res) => {
+  // make sure that the user thats deleting it its the owner of the post
+  Profile.findOne({user: req.user.id})
+  .then(profile => {
+    Post.findById(req.params.id)
+    .then(post => {
+      if (post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
+        return res.status(400).json({alreadyliked: 'User already liked this post'});
+      }
+      // Add user id to likes array
+      post.likes.unshift({user: req.user.id});
+      
+      post.save().then(post => res.json(post));
+    })
+    .catch(err => res.status(404).json({postnotfound: 'No post found'}));
+  })
+});
+// @route   Post to api/posts/unlike/:id
+// @desc    Unlike post
+// @access  Private
+router.post(
+  '/unlike/:id',passport.authenticate('jwt',{ session: false }), (req,res) => {
+  // make sure that the user thats deleting it its the owner of the post
+  Profile.findOne({user: req.user.id})
+  .then(profile => {
+    Post.findById(req.params.id)
+    .then(post => {
+      if (post.likes.filter(like => like.user.toString() === req.user.id).length === 0) {
+        return res.status(400).json({notliked: 'You have not yet liked this post'});
+      }
+      // Get remive index
+      const removeIndex = post.likes
+      .map(item => item.user.toString())
+      .indexOf(req.user.id);
+      
+      // Splice out of array
+      post.likes.splice(removeIndex, 1);
+      
+      // save
+      post.save().then(post => res.json(post));
+    })
+    .catch(err => res.status(404).json({postnotfound: 'No post found'}));
+  })
+});
+
+
 module.exports = router;
 
