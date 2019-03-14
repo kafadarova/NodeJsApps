b1109448b440bdef70f952ab9b8ab42a967516dc
kafadarova <k.kafadarova@gmail.com>
2019-03-14 16:08:17 +0100

    create login and register router and add link router instead a hfref

--- DevConnector/client/src/App.js
+++ DevConnector/client/src/App.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
+// as Router - instead using BrowseRouter - we will type Router
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+
 
 
 import './App.css';
@@ -9,11 +15,17 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-          <Landing />
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
           <Footer />
-      </div>
+        </div>
+      </Router>
     );
   }
 }
