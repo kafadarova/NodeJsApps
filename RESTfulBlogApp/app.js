b96b76d96728cd1c521828d15899ed729604c13d
kafadarova <k.kafadarova@gmail.com>
2019-06-01 23:22:53 +0200

    add a show route to display an item with its details

--- RESTfulBlogApp/app.js
+++ RESTfulBlogApp/app.js
@@ -57,6 +57,17 @@ app.post('/blogs', function(req,res) {
   })
 });
 
+// SHOW Route
+app.get('/blogs/:id', function(req,res){
+  Blog.findById(req.params.id, function(err, foundBlog) {
+    if (err) {
+      res.redirect('/blogs');
+    } else {
+      res.render('show', {blog: foundBlog});
+    }
+  }) 
+});
+
 const port = process.env.port || 3000;
 
 app.listen(port,() => {
