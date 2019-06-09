75ed82cdb3049ab1e2590b3278e2e661ce26d12e
kafadarova <k.kafadarova@gmail.com>
2019-06-09 18:23:15 +0200

    conenct with db, create a secret route

--- Authentication/AuthDemo/app.js
+++ Authentication/AuthDemo/app.js
@@ -1,8 +1,17 @@
-const express = requires('express');
+const express = require('express');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/auth_demo_app');
 
 const app = express();
 app.set('view engine', 'ejs');
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
   res.render('home');
-})
+});
+
+app.get('secret', (req, res) => {
+  res.render('secret');
+});
+app.listen(process.env.PORT, process.env.IP, () => {
+  console.log('server started ..');
+});
